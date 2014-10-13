f8be5f738f2b8f542e5e4705ae1397649cf42f4c
javier baos mora <jbaos@iota30.aulas.gsyc.es>
2014-10-13 16:23:50 +0200

    realizado el ejercicio 6 finalizado

--- karaoke.py
+++ karaoke.py
@@ -7,25 +7,47 @@ import sys
 import smallsmilhandler
 import os
 
-Entrada = sys.argv
 
-if __name__ == "__main__":
-    try:
+class KaraokeLocal(smallsmilhandler.SmallSMILHandler):
+
+    def __init__(self):
+        self.Entrada = sys.argv
         parser = make_parser()
         sHandler = smallsmilhandler.SmallSMILHandler()
         parser.setContentHandler(sHandler)
-        parser.parse(open(Entrada[1]))  
-        Lista = sHandler.get_tags()
-        for Elemento in Lista:
+        parser.parse(open(self.Entrada[1]))
+        self.Lista = sHandler.get_tags()
+
+    def __str__(self):
+        listastr = ""
+        for Elemento in self.Lista:
             Etiqueta = Elemento[0] + "\t"
             Atributos = ""
             for Atributo in Elemento[1].keys():
-                recurso = Elemento[1][Atributo]
-                if Atributo == "src" and Elemento[1][Atributo][0] == "h":
-                    os.system("wget -q " + recurso)
-                    recurso = Elemento[1][Atributo].split('/')
-                    recurso = recurso[-1]   
-                Atributos = Atributos + Atributo + '="' + recurso + '"' + '\t'
-            print Etiqueta + Atributos
-    except IndexError:
-        print ("Usage: python karaoke.py file.smil")
+                if Elemento[1][Atributo] != "":
+                    recurso = Elemento[1][Atributo]
+                    Atributos = Atributos + Atributo
+                    Atributos += '="' + recurso + '"' + '\t'
+            listastr += Etiqueta + Atributos + '\n'
+        return listastr
+
+    def do_local(self):
+        for Elemento in self.Lista:
+            Etiqueta = Elemento[0] + "\t"
+            for Atributo in Elemento[1].keys():
+                if Elemento[1][Atributo] != "":
+                    recurso = Elemento[1][Atributo]
+                    if Atributo == "src" and Elemento[1][Atributo][0] == "h":
+                        os.system("wget -q " + recurso)
+                        recurso = Elemento[1][Atributo].split('/')
+                        Elemento[1][Atributo] = recurso[-1]
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 2:
+        Karaoke = KaraokeLocal()
+        print Karaoke.__str__()
+        Karaoke.do_local()
+        print Karaoke.__str__()
+    else:
+        sys.exit('Usage: python karaoke.py file.smil')
