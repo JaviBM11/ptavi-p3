62e2873b4fdc8cb84808233b3727479a63bc812b
javier baos mora <jbaos@iota30.aulas.gsyc.es>
2014-10-13 16:25:56 +0200

    pep8 smallsmillhandler

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,56 +4,26 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
-        self.dicrootlayout = {}
-        self.dicregion = {}
-        self.dicimg = {}
-        self.dicaudio = {}
-        self.dictextstream = {}
+    def __init__(self):
         self.lista = []
-        self.etiquetas = ['root-layout', 'region']
-        
+        self.etiquetas = [
+            'root-layout', 'region', 'img', 'audio', 'textstream']
+        self.atributos = {
+            'root-layout': ["width", "height", "background-color"],
+            'region': ["id", "top", "bottom", "left", "right"],
+            'img': ["src", "region", "begin", "dur"],
+            'audio': ["src", "begin", "dur"],
+            'textstream': ["src", "region"]}
+
     def startElement(self, etiqueta, attrs):
         diccionario = {}
         if etiqueta in self.etiquetas:
-           for atributo in XXXXX(etiqueta):
+            for atributo in self.atributos[etiqueta]:
                 diccionario[atributo] = attrs.get(atributo, "")
-           self.lista.append([name, diccionario]
-
-        if etiqueta == 'root-layout':
-            self.dicrootlayout['width'] = attrs.get('width', "")
-            self.dicrootlayout['height'] = attrs.get('height', "")
-            self.dicrootlayout['background-color'] = attrs.get('background-color', "")
-            self.lista.append([etiqueta, self.dicrootlayout])
-            
-        elif etiqueta == 'region':
-            self.dicregion['id'] = attrs.get('id', "")
-            self.dicregion['top'] = attrs.get('top', "")
-            self.dicregion['bottom'] = attrs.get('bottom', "")
-            self.dicregion['left'] = attrs.get('left', "")
-            self.dicregion['right'] = attrs.get('right', "")
-            self.lista.append([etiqueta, self.dicregion])
-
-        elif etiqueta == 'img':
-            self.dicimg['src'] = attrs.get('src', "")
-            self.dicimg['region'] = attrs.get('region', "")
-            self.dicimg['begin'] = attrs.get('begin', "")
-            self.dicimg['end'] = attrs.get('end', "")
-            self.dicimg['dur'] = attrs.get('dur', "")
-            self.lista.append([etiqueta, self.dicimg])
-
-        elif etiqueta == 'audio':
-            self.dicaudio['src'] = attrs.get('src', "")
-            self.dicaudio['begin'] = attrs.get('begin', "")
-            self.dicaudio['dur'] = attrs.get('dur', "")
-            self.lista.append([etiqueta,self.dicaudio])
-
-        elif etiqueta == 'textstream':
-            self.dictextstream['src'] = attrs.get('src', "")
-            self.dictextstream['region'] = attrs.get('region', "")
-            self.lista.append([etiqueta, self.dictextstream])
+            self.lista.append([etiqueta, diccionario])
 
     def get_tags(self):
         return self.lista
@@ -62,5 +32,5 @@ if __name__ == "__main__":
     parser = make_parser()
     sHandler = SmallSMILHandler()
     parser.setContentHandler(sHandler)
-    parser.parse(open('karaoke.smil'))  
+    parser.parse(open('karaoke.smil'))
     print sHandler.get_tags()
