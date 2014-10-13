fe639ac9a2a0493eb2214f60e5f355b3fabc8f97
javier baos mora <jbaos@iota25.aulas.gsyc.es>
2014-10-13 10:04:05 +0200

    realizado el ejercicio 5 sin corregir

--- karaoke.py
+++ karaoke.py
@@ -5,6 +5,7 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import sys
 import smallsmilhandler
+import os
 
 Entrada = sys.argv
 
@@ -16,10 +17,15 @@ if __name__ == "__main__":
         parser.parse(open(Entrada[1]))  
         Lista = sHandler.get_tags()
         for Elemento in Lista:
-            Etiqueta = Elemento[0] + "/"
+            Etiqueta = Elemento[0] + "\t"
             Atributos = ""
             for Atributo in Elemento[1].keys():
-                Atributos = Atributos + Atributo + '="' + Elemento[1][Atributo] + '"' + '/'
+                recurso = Elemento[1][Atributo]
+                if Atributo == "src" and Elemento[1][Atributo][0] == "h":
+                    os.system("wget -q " + recurso)
+                    recurso = Elemento[1][Atributo].split('/')
+                    recurso = recurso[-1]   
+                Atributos = Atributos + Atributo + '="' + recurso + '"' + '\t'
             print Etiqueta + Atributos
     except IndexError:
         print ("Usage: python karaoke.py file.smil")
